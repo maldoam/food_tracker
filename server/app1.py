ddb636da6299b04bd83b91018f6a43eb07345b09
maldoam <53671601+maldoam@users.noreply.github.com>
2022-04-26 19:23:27 -0500

    test

--- server/app1.py
+++ server/app1.py
@@ -9,32 +9,32 @@ db = SQLAlchemy(app)
 CORS(app)
 
 class Event(db.Model):
-	product_name = db.Column(db.String(256)
-	ingredients_text = db.Column(db.String(4096), nullable = False)
+    product_name = db.Column(db.String(256))
+    ingredients_text = db.Column(db.String(4096), nullable = False)
 
-	def __repr__(self):
-		return f"Event: {self.description}"
+    def __repr__(self):
+        return f"Event: {self.description}"
 
-	def __init__(self, description):
-		self.description = description
+    def __init__(self, description):
+        self.description = description
 
 def format_event(event):
-	return {
-		"description": event.description, 
-		"id": event.id, 
-		"created_at": event.created_at
-	}
+    return {
+        "description": event.description, 
+        "id": event.id, 
+        "created_at": event.created_at
+    }
 
 
 # Get single Event
 @app.route("/api/events/<id>", methods = ["GET"])
 def get_event(id):
-	event = Event.query.(.all()
-	formatted_event = format_event(event)
+    event = db.session.execute("SELECT product_name FROM food_products WHERE ingredients_text LIKE '%Organic%'")
+    formatted_event = format_event(event)
 
-	return {"event": formatted_event}
+    return {"event": formatted_event}
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
 
