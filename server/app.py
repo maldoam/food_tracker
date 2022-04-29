b378731342e8c40720ebde6358827e9e9cb6fff6
maldoam <53671601+maldoam@users.noreply.github.com>
2022-04-28 20:29:20 -0500

    test

--- server/app.py
+++ server/app.py
@@ -9,34 +9,34 @@ db = SQLAlchemy(app)
 CORS(app)
 
 class Food(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	product_name = db.Column(db.String(256), nullable = False)
-	ingredients_text = db.Column(db.String(4096), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(256), nullable = False)
+    ingredients_text = db.Column(db.String(4096), nullable = False)
 
-	def __repr__(self):
-		return f"Food: {self.description}"
+    def __repr__(self):
+        return f"Food: {self.product_name}"
 
-	def __init__(self, description):
-		self.description = description
+    def __init__(self, product_name):
+        self.product_name = product_name
 
 def format_food(food):
-	return {
-		"id": food.id,
-		"product_name": food.product_name,
-		"ingredients_text": food.ingredients_text
-	}
+    return {
+        "id": food.id,
+        "product_name": food.product_name,
+        "ingredients_text": food.ingredients_text
+    }
 
 # Get Foods based on ingredient
 @app.route("/api/food/<ingredient>", methods = ["GET"])
 def get_foods(ingredient):
-	foods = db.query(Food).filter(Food.ingredients_text.like('%' + ingredient + '%')).all()
-	foods_list = []
+    foods = db.query(Food).filter(Food.ingredients_text.like('%' + ingredient + '%')).all()
+    foods_list = []
 
-	for food in foods:
-		foods_list.append(format_food(food))
+    for food in foods:
+        foods_list.append(format_food(food))
 
-	return {"foods": foods_list}
+    return {"foods": foods_list}
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
 
