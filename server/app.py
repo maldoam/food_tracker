7e6bcbb16ab57b896c452b9f7b922b6c8b951aeb
maldoam <53671601+maldoam@users.noreply.github.com>
2022-04-28 21:15:21 -0500

    more fixing?

--- server/app.py
+++ server/app.py
@@ -9,14 +9,19 @@ db = SQLAlchemy(app)
 
 CORS(app)
 
-class food_products(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+class Food(db.Model):
+    __tablename__ = "food_products"
+
     product_name = db.Column(db.String(256), nullable = False)
     ingredients_text = db.Column(db.String(4096), nullable = False)
+    id = db.Column(db.Integer, primary_key = True)
 
     def __init__(self, product_name, ingredients_text):
         self.product_name = product_name
         self.ingredients_text = ingredients_text
+
+    def __repr__(self):
+        return f"id: {self.id}, product_name: {self.product_name}"
 
 def format_food(food):
     return {
@@ -28,7 +33,7 @@ def format_food(food):
 # Get Foods based on Ingredient entered
 @app.route("/api/food/<ingredient>", methods = ["GET"])
 def get_foods(ingredient):
-    foods = db.query(food_products).filter(food_products.ingredients_text.like('%' + ingredient + '%')).all()
+    foods = db.query(Food).filter(Food.ingredients_text.like('%' + ingredient + '%')).all()
     foods_list = []
 
     for food in foods:
